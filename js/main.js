c25d665c2b24f63f30438cc36f6f47a91ec126c8
MishkoGo <103243560+MishkoGo@users.noreply.github.com>
2023-05-31 19:47:15 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 const writeForm = document.getElementById('calculate-form'),
+      removeForm = document.getElementById('demo'),   
       formaHass = document.getElementById('uncompleted-todos'),
       formtextt = document.getElementById('todo-input');
 
-let form = document.querySelector('.todos');
-
-document.querySelector('#icon').onclick = () => {
-    form.classList.remove('active');
-}
-
 const writing = (e) => {
     e.preventDefault()
-    
 
     let div = document.createElement('div');
     div.className = "todo-item";
-    div.innerHTML = `<span><input type='checkbox'>${formtextt.value}</span>
-    <a href="#" class="a-item">
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    div.innerHTML = `
+                    <span><input type='checkbox'>${formtextt.value}</span>
+                    <a href="#" class="a-item"  onclick="func()">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
         
-        <path d="M18 6l-12 12" />
-        <path d="M6 6l12 12" />
-    </svg>
-</a>`;
-    
-    
+                            <path d="M18 6l-12 12" />
+                            <path d="M6 6l12 12" />
+                        </svg>
+                    </a>
+                    `;
+                    
     formaHass.append(div);
+    formtextt.value = "";
 }
 
-
+function func(){
+    let div = document.querySelector('.todo-item');
+    div.remove();
+}
 
 writeForm.addEventListener('submit', writing)
