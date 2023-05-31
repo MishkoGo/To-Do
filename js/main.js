dfaac2fc56a29cb5caa854cff2ab746d7418d3df
MishkoGo <103243560+MishkoGo@users.noreply.github.com>
2023-05-31 17:21:15 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 const writeForm = document.getElementById('calculate-form'),
       formaHass = document.getElementById('uncompleted-todos'),
-      formtextt = document.getElementById('todo-input')
+      formtextt = document.getElementById('todo-input');
+
+let form = document.querySelector('.todos');
+
+document.querySelector('#icon').onclick = () => {
+    form.classList.remove('active');
+}
 
 const writing = (e) => {
     e.preventDefault()
     
 
     let div = document.createElement('div');
-   
     div.className = "todo-item";
-    div.innerHTML = `<span><input type='checkbox'>${formtextt.value}</span>`;
+    div.innerHTML = `<span><input type='checkbox'>${formtextt.value}</span>
+    <a href="#" class="a-item">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        
+        <path d="M18 6l-12 12" />
+        <path d="M6 6l12 12" />
+    </svg>
+</a>`;
+    
+    
     formaHass.append(div);
 }
+
+
 
 writeForm.addEventListener('submit', writing)
